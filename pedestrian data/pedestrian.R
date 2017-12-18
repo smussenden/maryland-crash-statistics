748614461962bf78ca6ea2b00c42e386e737c92f
Jake Gluck <jagluck@terpmail.umd.edu>
2017-12-18 16:16:22 -0500

    remove

--- pedestrian data/pedestrian.R
+++ pedestrian data/pedestrian.R
@@ -8,10 +8,12 @@ rm(list=ls())
 # install dbscan package, which is the clustering algo we'll use to identify pedestrian crash hotspots
 install.packages("fpc")
 install.packages("tidyverse")
+install.packages("sqldf")
 
 # load library
 library(fpc)
 library(tidyverse)
+library(sqldf)
 
 
 # Import Maryland State Police vehicle crash data.  Raw data is here: https://github.com/Capital-News-Service/maryland-crash-statistics, with data dictionary. These were originally stored as Excel files with multipel sheets.  We've stripped out those sheets into separate CSVs and then read in.  We're just reading in crash tables and person tables for each quarter.  Note that we're removing certain fields on read in to ensure that 2016 and 2017 fields match 2015 fields. 
@@ -111,8 +113,16 @@ rm(person62)
 rm(person63)
 rm(person64)
 
+select <-person %>%
+  group_by(REPORT_NO) %>%
+  filter(PERSON_TYPE != "D")
+
+select = sqldf("SELECT * from person
+      GROUP BY PERSON.REPORT_NO
+      HAVING (PERSON.PERSON_TYPE != 'D')")
+
 # Check the range of values in PERSON_TYPE - total of 11982 P
-person_type_count <- person %>%
+person_type_count <- select %>%
   group_by(PERSON_TYPE) %>%
   summarise(count=n()) 
 View(person_type_count)
@@ -129,39 +139,51 @@ person_count <- combine %>%
   group_by(REPORT_NO, PERSON_ID) %>%
   summarise(count=n()) 
 
-# Remove three copies of this problematic record. This results in us having, in the combined file, 11980 records, which is why it's 2 less records in combine than in person. 
-combine <- combine %>% distinct()
+# Remove three copies of this problematic record. This results in us having, in the combined file, 11981 records.
 
 combine <- combine %>%
-  filter(REPORT_NO != 'MCP21950009')
+  filter(row_number() != 9575) %>%
+  filter(row_number() != 9576) %>%
+  filter(row_number() != 9577) 
 
-#sorts out only pedestrian accidents
-pedestrian_large <- subset(combine, PERSON_TYPE=="P")
+# Before filtering for SHA-only roads, look at range of ROUTE_TYPE_CODES.  Note that Of the 11,981 pedestrian crashes, 3835 have no route type given.  An analysis of these null value records found that, though there was some geographic information, there was simply not enough information provided in the data set to say with any certainty that the crashes occured on state, county, US or other roads, or private parking lots. Some of them appeared to have been located on or near US or state roads.  Note that this means that the true number of pedestrian crashes on US and Maryland roads is likely to be higher, but that we can't say that with certainty.  Our analysis must be couched as "at least" and acknowledge these flaws.    
 
-#create subset for state and us highways
-us_roads <- subset(pedestrian_large, ROUTE_TYPE_CODE=="US")
-state_roads <- subset(pedestrian_large, ROUTE_TYPE_CODE=="MD")
+road_type_count <- combine %>%
+  group_by(ROUTE_TYPE_CODE) %>%
+  summarise(count=n()) 
+
+no_route_code <- combine %>%
+  filter(ROUTE_TYPE_CODE == "")
+
+no_route_code_mainroad <- no_route_code %>%
+  subset(COUNTY_NO!=24) %>%
+  subset(MAINROAD_NAME != "")
+
+
+# Create subset for state and us highways.  Note that there were 103 records for IS (Insterstate Route), but a decision was made to leave those out becuase of questions about SHA vs other agency control of different parts of those roads.  
+us_roads <- subset(combine, ROUTE_TYPE_CODE=="US") # 2495 people in crashes
+state_roads <- subset(combine, ROUTE_TYPE_CODE=="MD") # 609 people in crashes
 
 #combine US and state roads into one table
 us_and_stater = rbind(us_roads, state_roads)
 
 
 #extracts only fields we want
-pedestrian <- pedestrian_large[c(1,3,12,13,14,20,21,22,23,27,34,35,36,39,44,46,56,57)]
-us_and_stater = us_and_stater[c(1,3,12,13,14,20,21,22,23,27,34,35,36,39,44,46,56,57)]
+pedestrian <- combine[c(1,3,12,13,14,20,21,22,23,27,34,35,36,39,44,46,56,57)]
+us_and_stater <- us_and_stater[c(1,3,12,13,14,20,21,22,23,27,34,35,36,39,44,46,56,57)]
 
 #removes row names
 rownames(pedestrian) <- c()
 rownames(us_and_stater) <- c()
 
-#remove baltimore data
+#remove Baltimore city data because SHA doesn't maintain roads in Baltimore City
 us_and_state_nobmore <- subset(us_and_stater, COUNTY_NO!=24)
 
 #write data to csv file, this is for the us and state no baltimore
 write.csv(pedestrian, file = "us_and_state_nobmore.csv")
 
 #write data to csv file, this is for the entire file
-#write.csv(pedestrian, file = "pedestrian.csv")
+write.csv(pedestrian, file = "pedestrian.csv")
 
 #write data to csv file, this is for just US and MD roads
 #write.csv(us_and_stater, file = "pedestrian_state_and_us.csv")
@@ -181,7 +203,7 @@ data2=data
 # plot points
 plot(data2)
 
-# run DBSCAN clustering
+# run DBSCAN clustering.  It takes two core arguments.  The first is MinPts, which is the minimum number of points in a cluster.  The second is the epsilon neighborhood (EPS), which is a proxy for density.  It says that for a point to be included, it must be within .005 (accounting for degrees lat and long) of core points of that cluster. FOR MORE: https://www.aaai.org/Papers/KDD/1996/KDD96-037.pdf
 ds <- dbscan(data2, eps=.005, MinPts=5, showplot=1)
 
 # create a vector with the cluster prediction
@@ -194,16 +216,11 @@ exp <- data.frame(data,pre)
 #exp <- subset(exp, pre>87)
 #exp <- subset(exp, pre<89)
 
-#add information back to cluster
-cluster = merge(us_and_state_nobmore, exp, by= c("LATITUDE", "LONGITUDE"))
 
-#remove duplicate rows
-cluster = unique(cluster)
-
-#remove noise points
-cluster <- subset(cluster, pre!=0)
+#remove noise points that are not located in any cluster. That gives us 1157 persons in clusters
+exp <- subset(exp, pre!=0)
 
 # write to csv
-write.table(cluster, "clusters.csv", sep=",", row.names = F)
+write.table(exp, "clusters.csv", sep=",", row.names = F)
 
 
